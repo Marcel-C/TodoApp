2b8517b97e57d5d0904a2c8f4a297d46e07f332c
marcioba1 <marcel.ciobanu@publicisresources.com>
2025-04-07 11:20:51 +0300

    First commit

--- main.py
+++ main.py
@@ -10,13 +10,14 @@ while True:
             todos.append(todo)
         case 'show':
             for index, item in enumerate(todos):
-                row = f"{index}:{item}"
+                row = f"{index}-{item}"
                 print(row)
         case 'edit':
             number = int(input("Number of the todo to edit: "))
             number = number - 1
             new_todo = input("Enter a new todo: ")
             todos[number] = new_todo
+
         case 'exit':
             break
 
